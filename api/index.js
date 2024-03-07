7bd5578fd2216d11c5d85f7f02df57d28511b055
aman13e <amanuelaweke13@gmail.com>
2024-03-07 19:21:44 +0300

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
